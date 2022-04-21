071fc7728d19d07386c89bf008cb91dfa4947146
Magnus1515 <82829595+Magnus1515@users.noreply.github.com>
2022-04-21 00:03:12 -0700

    Add files via upload

---
+++ HashTableFinal.py
@@ -0,0 +1,24 @@
+
+def Word(palabra): 
+	arr = palabra.split()
+	print(arr)
+
+	for x in arr:		
+		print(a+x == arr[i])
+
+
+
+a = hash("Red")
+b = hash("Green")
+c = hash("Blue") 
+
+divide1 = a % 8
+divide2 = b % 8 
+divide3 = c % 8
+
+#print(divide1, divide2, divide3)
+
+
+H = Word("todos los dias ")
+print(H)
+
